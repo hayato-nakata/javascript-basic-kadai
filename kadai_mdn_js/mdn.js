9f5e772d86a3fb02518aaea61b93cc247b7548f3
hayato-nakata <balcerona4510@gmail.com>
2024-10-03 01:07:21 +0900

    kadai_mdn_jsの提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const year = now.getFullYear();
+const lastMonth = now.getMonth();
+const thisMonth = lastMonth + 1
+const date = now.getDate();
+
+console.log(year + '年' + thisMonth + '月' + date + '日') ; 
+
+
